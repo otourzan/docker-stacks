385f34e847f9df35761eabab77010f52ac7278a5
Omid Tourzan <oto@chromium.org>
2021-06-02 18:10:46 +1000

    Restrict configuration based on GCP acccess

--- gcp-ai-notebook/jupyter_notebook_config.py
+++ gcp-ai-notebook/jupyter_notebook_config.py
@@ -13,7 +13,9 @@ c.NotebookApp.port = 8080
 c.NotebookApp.open_browser = False
 c.NotebookApp.token = ''
 c.NotebookApp.password = ''
-c.NotebookApp.allow_origin= '*'
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.disable_check_xsrf = False
+c.NotebookApp.allow_origin_pat = '(^https://8080-dot-[0-9]+-dot-devshell\.appspot\.com$)|(^https://colab\.research\.google\.com$)|((https?://)?[0-9a-z]+-dot-datalab-vm[\-0-9a-z]*.googleusercontent.com)|((https?://)?[0-9a-z]+-dot-[\-0-9a-z]*.notebooks.googleusercontent.com)|((https?://)?[0-9a-z\-]+\.[0-9a-z\-]+\.cloudshell\.dev)|((https?://)ssh\.cloud\.google\.com/devshell)'
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
